5b8caa29ded84881f5b836d93187cc70d89aeff2
Danaipong <fluke.danaipong@gmail.com>
2023-01-11 16:22:29 +0700

    First Commit

---
+++ GitHub Command/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello GitHub")
+}
+
+//git remote add origin https://github.com/Flxke15/GitCommandPratice.git
---
+++ go.mod
@@ -0,0 +1,3 @@
+module GitCommandtesing
+
+go 1.19
